b76a30958d325552c8b13638d1d5ea6449f961e3
ehsan2793 <ehsan2793@gmail.com>
2021-10-05 23:02:01 -0700

    model function are complete

---
+++ api/monsters/monster-model.js
@@ -0,0 +1,29 @@
+const db = require('../../data/connect');
+
+const getAll = () => {
+    return db('monsters');
+};
+const getById = async (id) => {
+    const monster = await db('monsters').where({ id: id });
+    return monster;
+};
+const insert = async (monster) => {
+    const [id] = await db('monsters').insert(monster);
+    return getById(id);
+};
+const updateById = async (id, monster) => {
+    await db('monsters').where({ id: id }).update(monster);
+};
+const removeById = async (id) => {
+    const deletedMonster = getById(id);
+    await db('monsters').where({ id: id }).del();
+    return deletedMonster;
+};
+
+module.exports = {
+    getAll,
+    getById,
+    insert,
+    updateById,
+    removeById,
+};
