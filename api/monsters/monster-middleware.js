3449b25dcb9a33799b607047492288eb3bde38cb
ehsan2793 <ehsan2793@gmail.com>
2021-10-06 00:17:32 -0700

    check id middleware setup complete

--- api/monsters/monster-middleware.js
+++ api/monsters/monster-middleware.js
@@ -1,5 +1,19 @@
+const { getById } = require('./monster-model');
+
 const checkId = async (req, res, next) => {
-    next();
+    try {
+        const idExist = await getById(req.params.id);
+        if (!idExist) {
+            next({
+                status: 404,
+                message: `monster with the id of ${req.params.id} does not exist`,
+            });
+        } else {
+            next();
+        }
+    } catch (error) {
+        next(error);
+    }
 };
 const checkpayload = async (req, res, next) => {
     next();
