1133085a42f92f8b180d3145d14c8b408986422a
ehsan2793 <ehsan2793@gmail.com>
2021-10-06 00:09:19 -0700

    middleware are created and exported

---
+++ api/monsters/monster-middleware.js
@@ -0,0 +1,15 @@
+const checkId = async (req, res, next) => {
+    next();
+};
+const checkpayload = async (req, res, next) => {
+    next();
+};
+const monsterIsUnique = async (req, res, next) => {
+    next();
+};
+
+module.exports = {
+    checkId,
+    checkpayload,
+    monsterIsUnique,
+};
--- api/monsters/monster-router.js
+++ api/monsters/monster-router.js
@@ -1,29 +1,33 @@
 const express = require('express');
-const mb = require('./monster-model');
-
 const router = express.Router();
+const mb = require('./monster-model');
+const {
+    checkId,
+    checkpayload,
+    monsterIsUnique,
+} = require('./monster-middleware');
 
 router.get('/', async (req, res) => {
     const allMonsters = await mb.getAll();
     res.status(200).json(allMonsters);
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', checkId, async (req, res) => {
     const monster = await mb.getById(req.params.id);
     res.status(200).json(monster);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', checkpayload, monsterIsUnique, async (req, res) => {
     const newMonster = await mb.insert(req.body);
     res.status(201).json(newMonster);
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', checkId, checkpayload, monsterIsUnique, async (req, res) => {
     const updatedMonster = await mb.updateById(req.params.id, req.body);
     res.status(200).json(updatedMonster);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', checkId, async (req, res) => {
     const deletedMonster = await mb.removeById(req.params.id);
     res.status(200).json(deletedMonster);
 });
