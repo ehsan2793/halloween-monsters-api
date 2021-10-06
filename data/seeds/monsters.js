27998e756ce95be82aa6309e35572a7d0ae751ab
ehsan2793 <ehsan2793@gmail.com>
2021-10-05 22:50:16 -0700

    inital data are inserted into table

--- data/seeds/monsters.js
+++ data/seeds/monsters.js
@@ -1,13 +1,11 @@
+const monsters = require('../../initialMonsters');
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').truncate()
+  return knex('monsters')
+    .truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        { id: 1, colName: 'rowValue1' },
-        { id: 2, colName: 'rowValue2' },
-        { id: 3, colName: 'rowValue3' }
-      ]);
+      return knex('monsters').insert(monsters);
     });
 };
---
+++ initialMonsters.js
@@ -0,0 +1,74 @@
+const monsters = [
+    {
+        Monster_name: 'Vampire',
+        Description:
+            'a creature from folklore that subsists by feeding on the vital essence of the living',
+    },
+    {
+        Monster_name: 'Zombie',
+        Description:
+            'mythological undead corporeal revenant created through the reanimation of a corpse.',
+    },
+    {
+        Monster_name: 'Mummy',
+        Description:
+            'dead human or an animal whose soft tissues and organs have been preserved by either intentional or accidental exposure to chemicals.',
+    },
+    {
+        Monster_name: 'Ghost',
+        Description:
+            'a ghost is the soul or spirit of a dead person or animal that can appear to the living.',
+    },
+    {
+        Monster_name: 'werewolf',
+        Description:
+            'a human with the ability to shapeshift into a wolf, either purposely or after being placed under a curse or affliction with the transformations occurring on the night of a full moon.',
+    },
+    {
+        Monster_name: 'Witch',
+        Description:
+            'a person and especially a woman believed to have magic powers.',
+    },
+    {
+        Monster_name: 'The Grim Reaper',
+        Description:
+            'Death is frequently imagined as a personified force. In some mythologies, a character known as the Grim Reaper',
+    },
+    {
+        Monster_name: 'The Headless Horseman',
+        Description:
+            'The Headless Horseman is a mythical figure who has appeared in folklore around the world since the Middle Ages.',
+    },
+    {
+        Monster_name: 'Frankenstein',
+        Description:
+            'Victor Frankenstein, a young scientist who creates a sapient creature in an unorthodox scientific experiment',
+    },
+    {
+        Monster_name: 'Swamp monster',
+        Description:
+            'A swamp monster is a fictional creature often imagined to lurk in a swamp',
+    },
+    {
+        Monster_name: 'Evil clowns',
+        Description:
+            'The evil clown is a subversion of the traditional comic clown character, in which the playful trope is instead depicted in a more disturbing nature through the use of horror elements and dark humor.',
+    },
+    {
+        Monster_name: 'Demon',
+        Description:
+            'A demon is a supernatural being, typically associated with evil, prevalent historically in religion.',
+    },
+    {
+        Monster_name: 'Killer robots',
+        Description:
+            'Lethal autonomous weapons are a type of autonomous military system that can independently search for and engage targets based on programmed constraints and descriptions.',
+    },
+    {
+        Monster_name: 'Aliens ',
+        Description:
+            'Extraterrestrial life is hypothetical life that may occur outside Earth and which did not originate on Earth.',
+    },
+];
+
+module.exports = monsters;
