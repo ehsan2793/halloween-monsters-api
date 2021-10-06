e2d3502257242821f77b4e90c96d4815734dcf7c
ehsan2793 <ehsan2793@gmail.com>
2021-10-05 23:48:23 -0700

    all mosnter can be viewed at the endpoint

--- api/monsters/monster-router.js
+++ api/monsters/monster-router.js
@@ -1,10 +1,28 @@
 const express = require('express')
-
+const mb = require('./monster-model');
 
 
 const router = express.Router()
 
+router.get('/', async (req, res) => {
+    const allMonsters = await mb.getAll()
+    res.status(200).json(allMonsters)
+})
 
 
 
+
+
+
+
+//eslint-disable-next-line
+router.use("*", (err, req, res, next) => {
+    res.status(err.status || 500).json(
+        {
+            message: err.message ? err.message : "something went wrong"
+
+        }
+    )
+
+})
 module.exports = router
