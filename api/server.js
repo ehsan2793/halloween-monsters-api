bc77a2334392e382f855aa98320bf987d429070e
ehsan2793 <ehsan2793@gmail.com>
2021-10-05 21:42:14 -0700

    router step up is complete

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const monstersRouter = require('./monsters/monster-router');
 
 
 const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
-
-
+server.use('/api/monsters', monstersRouter);
 
 module.exports = server;
