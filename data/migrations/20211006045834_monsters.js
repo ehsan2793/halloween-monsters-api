97f769709cf9d2517b7f4d7fde8ffd7a70fd24fa
ehsan2793 <ehsan2793@gmail.com>
2021-10-05 22:49:51 -0700

    inital data are inserted into table

--- data/migrations/20211006045834_monsters.js
+++ data/migrations/20211006045834_monsters.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
     return knex.schema.createTable('monsters', (table) => {
         table.increments('monster_id');
         table.string('Monster_name', 30).notNullable().unique();
-        table.string('Description').notNullable();
+        table.string('Description', 500).notNullable();
     });
 };
 
